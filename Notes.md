a790a3b224b22a8aa379002422759ad07e32531d
Milad MKL <miladmiyakheel@gmail.com>
2022-07-05 21:23:13 +0200

    make it better

---
+++ Notes.md
@@ -0,0 +1,6 @@
+npm install express
+
+npm install --save-dev nodemon
+
+Um Zugang vom Frontend zu erlauben:
+npm install cors
--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
+
 
 let tasks = [
   {
     id: 1,
-    name: "HTML is easy",
+    title: "HTML is easy",
     date: "2022-05-30T17:30:31.098Z",
     completed: false
   },
   {
     id: 2,
-    name: "CSS is easy",
+    title: "CSS is easy",
     date: "2022-05-30T18:39:34.091Z",
     completed: false
   },
   {
     id: 3,
-    name: "Javascript is difficult",
+    title: "Javascript is difficult",
     date: "2022-05-30T19:20:14.298Z",
     completed: false
   },
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(tasks));
+app.get('/', (request, response) => {
+  response.send('<h1>Hello there user!</h1>')
+})
+
+app.get('/api/tasks', (request, response) => {
+  response.json(tasks)
+})
+
+app.get('/api/tasks/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const task = tasks.find(task => task.id === id)
+
+  if (task) {
+    response.json(task)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/tasks/:id', (request, response) => {
+  const id = Number(request.params.id)
+  tasks = tasks.filter(task => task.id !== id) // Überschreibe tasks mit einer neuen Liste
+  response.status(204).end()
+})
+
+app.post('/api/tasks', (request, response) => {
+  const task = request.body
+  console.log(task)
+  response.json(task)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running at http://localhost:${PORT}/`)
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
